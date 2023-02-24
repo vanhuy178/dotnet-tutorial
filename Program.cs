4f2e54f61338eca0ec9c13772cb3049e3de65252
HUY_FA <nguyenvanhuy178tiengiang@gmail.com>
2023-02-25 02:30:21 +0700

    polyphisum

--- Program.cs
+++ Program.cs
@@ -3,12 +3,47 @@ using System;
 
 namespace MyApplication
 {
-    class Car
-    {
-        // field
-        // fiels
 
-        // method
-        public Car() { } //constructor
+    class Animal
+    {
+        public virtual void animalSound()
+        {
+            Console.WriteLine("The animal makes a sounds");
+        }
+    }
+    class Pig : Animal  // Derived class (child) 
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+
+
+    class Dog : Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The pig says: gau gau!!!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();
+            Animal myPig = new Pig();
+            Animal myDog = new Dog();
+
+            myAnimal.animalSound();
+            myPig.animalSound();
+            myDog.animalSound();
+
+            Pig heonai = new Pig();
+            Animal heocon = new Pig();
+
+            Console.WriteLine("Heo con kêu");
+            heocon.animalSound();
+        }
     }
 }
